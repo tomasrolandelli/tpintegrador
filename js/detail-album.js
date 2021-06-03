4b074c7e4585a1d0e90804840a1366bce0fa54e7
ClaraOttonello <ClaritaOttonello@MacBook-de-Juliana.local>
2021-06-03 13:25:51 -0300

    termino detalle

--- js/detail-album.js
+++ js/detail-album.js
@@ -3,18 +3,16 @@ window.addEventListener("load", function(){
   console.log(window.location.search);
 let info= document.querySelector("#datosPortada ");
 let canciones= document.querySelector(".canciones");
-let image= document.querySelector("#image");
+
 
 let queryString= location.search;
-let queryStringObj= new URLSearchParams(queryString);//modelo de obj en js
+let queryStringObj= new URLSearchParams(queryString);
 let busqueda= queryStringObj.get('id');
 
-
-//este si*/
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${busqueda}`)
 
 .then(function(response){
-    console.log(response)
+  console.log(response)
 return response.json()
 })
 .then(function(datos){
@@ -23,20 +21,66 @@ let nombreAlbum= datos.title
 console.log(nombreAlbum)
 let artistaAlbum= datos.artist.name
 let imagenAlbum=datos.cover_medium
+let imagenArtista= datos.artist.picture_small
+let releaseDate= datos.release_date
 info.innerHTML +=  
+` 
+<article class="portada">
+<figure class="banner"> <img src= "${imagenAlbum}"alt="${nombreAlbum}"></figure> 
+<div class="nn" >
+   <h1 id="album">${nombreAlbum}</h1>
+            
+<div class="info-avicii">
+  <div>
+    <figure><img src="${imagenArtista}"></figure>    
+     <h5 id="artista">${artistaAlbum}</h5>
+  </div> 
+  <h6><time datetime="${releaseDate}">${releaseDate}</time></h6>         
+</div>
+<div >
+</article>`
 
-` <figure class="banner"> <img src= "${imagenAlbum}"alt="${nombreAlbum}"></figure>           
-<div class="info">
-<h1>${nombreAlbum}</h1>
+
+})
+ .catch(function (error) {
+console.log(error)
+})
+
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${busqueda}`)
+.then(function (respuesta) {
+console.log(respuesta)
+return respuesta.json()
+})
+.then(function (data) {
+  let nombreDelAlbum= data.title
+  let imagenn=data.cover_small
+  let album= data.tracks.data
+console.log(album)
+for (let i=0; i<album.length; i++){
+let nombreCancion= album[i].title
+
+ canciones.innerHTML= canciones.innerHTML +
+` <article class="track">
+<a href="./playlist.html" class="corazon"><i class="far fa-heart"></i></a>  
+<a href="./detail-track.html"><img src= "${imagenn}" alt="${nombreDelAlbum}"></a>
 <div>
-<figure><img url= ""></figure>    
-<h5>${artistaAlbum}</h5>
-</div>   
-</div>`
-image.innetHTML+= `<a href="detail-track.html" > <img src="${imagenAlbum}" alt="${nombreAlbum}"></a>`
+   <a href="detail-track.html"><h2>${nombreCancion}</h2></a>                    
+</div>    
+<a href="detail-track.html" class="punto"><i class="fas fa-ellipsis-h"></i></a> 
+</article>`
+ 
+
+}
+})
+
+.catch(function (error) {
+console.log(error)
+})
 
 
 
+
+})
 /*
 ESTE NO
 fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart')
@@ -71,46 +115,6 @@ let imagenAlbum= albums[i].artist.picture_medium
     </div>`
      
     }*/
-})
- .catch(function (error) {
-console.log(error)
-})
-
-fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${busqueda}`)
-.then(function (respuesta) {
-console.log(respuesta)
-return respuesta.json()
-})
-.then(function (data) {
-  
-     let album= data.tracks.data
-console.log(album)
-for (let i=0; i<album.length; i++){
-let nombreCancion= album[i].title
-let imagen= album[i]
-
- canciones.innerHTML= canciones.innerHTML +
-` <article class="track">
-<a href="./playlist.html" class="corazon"><i class="far fa-heart"></i></a>  
-
-<div>
-   <a href="detail-track.html"><h2>${nombreCancion}</h2></a>                    
-</div>    
-<a href="detail-track.html" class="punto"><i class="fas fa-ellipsis-h"></i></a> 
-</article>`
- 
-
-}
-})
-
-.catch(function (error) {
-console.log(error)
-})
-
-
-
-
-})
 /*
 `  <a href="./playlist.html" class="corazon"><i class="far fa-heart"></i></a>  
  <a href="./detail-track.html"></a>
