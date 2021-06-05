5e27f02432a56f8c1825c46348f695c8a9443a3b
Nour Awada <nourawada@MacBook-Pro-de-Nour.local>
2021-06-05 14:18:46 -0300

    correcion estetica detail-track

--- js/detail-track.js
+++ js/detail-track.js
@@ -5,10 +5,11 @@ window.addEventListener("load", function () {
     let queryString = location.search;
     let queryStringObj = new URLSearchParams(queryString)
     let busqueda = queryStringObj.get('id');
+    let urlTracks = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${busqueda}`
 
     let track = document.querySelector("#tracksnour")
 
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${busqueda}`)
+    fetch(urlTracks)
         .then(function (response) {
             console.log(response)
             return response.json()
@@ -18,15 +19,26 @@ window.addEventListener("load", function () {
 
             let nombreTrack = datos.title
             let autorTrack = datos.artist.name
-            let imagenTrack = datos.artist.picture_small
+            let imagenTrack = datos.artist.picture_medium
+            let albumTrack = datos.album.title
+            let linkTrack = datos.preview
             console.log(nombreTrack);
             console.log(autorTrack);
             console.log(imagenTrack);
+            console.log(albumTrack);
+            console.log(linkTrack);
+            
 
             track.innerHTML += 
-            `<a href="./detail-track.html"><img src="${imagenTrack}" alt="${nombreTrack}"></a>
-            <a href="./detail-track.html"><h1>${autorTrack}<h1>
-            <h3>${nombreTrack}</h3>
+            `<a href="./detail-track.html"><div class= "divtrack"><div class= "divtrackhijo"><img class= "imgtrack" src="${imagenTrack}" alt="${nombreTrack}"></a>
+            <a href="./detail-track.html"><h1 class= "headtrack" id="h1track">${nombreTrack}<h1>
+            <h3 class= "headtrack" id="h3track">${albumTrack}</h3>
+            <h4 class= "headtrack">${autorTrack}</h4>
+            <audio controls class="audiotrack">
+            <source src="${linkTrack}">
+            </audio>
+            </div>
+            </div>
             </a>`
         })
         .catch(function (error) {
